8dfb1b77c605f8a1dd72fa264c5f263b8a6ec12f
Bloo Printers <deploy@blooprinters.com>
2026-02-13 11:29:14 +0100

    Fix login page syntax error and update task status

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { loginAction } from '../actions'
 import { GoogleSignInButton } from '@/components/auth/GoogleSignInButton'
 
 export default function LoginPage() {
@@ -12,18 +11,13 @@ export default function LoginPage() {
                         Sign in to your account
                     </h2>
                     <p className="mt-2 text-sm text-gray-600">
-                        For MVP: Use admin@bloo.com / subscriber@bloo.com / guest@bloo.com
-                        <br />
-                        Password is 'hashedpassword'
+                        Secure access via Google
                     </p>
                 </div>
                 <div className="mt-8">
                     <GoogleSignInButton />
-
-                    <div className="mt-8">
-                        <GoogleSignInButton />
-                    </div>
                 </div>
             </div>
-            )
+        </div>
+    )
 }
